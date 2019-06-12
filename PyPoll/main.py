9d2fc01f5cb1b29c94a1d3e1b6bfd5c6cc8486cd
datamoa <irisshowy@gmail.com>
2019-06-11 18:34:08 -0700

    pushed updated main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,28 +7,31 @@ file = ('03-Python_homework_PyPoll_Resources_election_data.csv')
 # Give a title to all outputs
 print("Election Results")
 
+#Path to atore results as a txt file
 result_file = open("result.txt","w")
-
 
 # open the CSV
 with open(file, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header_row = next(csvreader)
+   #set an empty list for Voter total and Candidate list
     voters = []
     candidate = []
+    #Loop to add items to an empty list
     for row in csvreader:
         voters.append(row[0])
         candidate.append(row[2])
-
+    #Find and print total voters
     total_voters = len(voters)
     print("Total voters: " + str(total_voters))
 
+    #Use counter function to find candidate occurancies and set dictionary 
     candidate_list = Counter(candidate)
     results = candidate_list.items()
-
+    #Set a string for winner and start winner count as O
     winner_name = ""
     winner_count = 0
-
+    #Use loop to find pecentage in dic
     for item in results:
         percent = round((item[1] / total_voters) * 100)
         line = item[0] + ': ' + str(percent) + '%' + ', ' + str(item[1]) +"\n"
@@ -40,7 +43,7 @@ with open(file, newline="") as csvfile:
             
     print('Winner is ' + winner_name)
     
-# # py_poll = open("file.txt", "w")
+#  py_poll = open("file.txt", "w")
 # save_py_poll = ["str1\n",
 #     "Election Results" + "\n",
 #     "Total voters: " + str(total_voters) + "\n",
